3f03daf7dfb135cf77601bd680f9a28606eaf440
Ryan Pimiskern <rpimiskern@gmail.com>
2025-07-12 21:23:15 -0600

    acquire test data

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+import * as fs from "fs/promises";
+import { FeatureCollection, Point } from "geojson";
+
+type AQDataProps = { name: string; type: string; pm25: number; validTime: Date };
+type AQData = FeatureCollection<Point, AQDataProps>;
+type APIResponse = {
+  status: "success";
+  data: AQData;
+};
+
 async function main() {
-  console.log("Hello, world!");
+  const testData = (JSON.parse(await fs.readFile("./testData/aq-data.geojson", "utf-8")) as APIResponse).data;
+
+  console.log(testData.features.length, "features loaded.");
 }
 
 await main();
-
